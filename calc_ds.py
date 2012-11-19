0d66433932586570792e73977ea4056c71941d74
eljost <jsteinmetzer1108@aol.com>
2012-11-19 10:33:28 +0100

    Added support for floating point numbers in formulas

--- calc_ds.py
+++ calc_ds.py
@@ -25,14 +25,14 @@ TOKENS = {"H" : 1.0079,
 def parse_formula(raw_formula):
     formula = list()
     # Split by element
-    formula_split = re.findall("([A-Z][a-z]?\d*)", raw_formula)
+    formula_split = re.findall("([A-Z][a-z]?[\d\.]*)", raw_formula)
     for item in formula_split:
         # Get the stoichiometric coefficients (count)
-        element, count = re.match("([A-Za-z]+)(\d*)", item).groups()
+        element, count = re.match("([A-Za-z]+)([\d\.]*)", item).groups()
         if count is "":
             count = 1
         else:
-            count = int(count)
+            count = float(count)
         formula.append((element, count))
 
     return formula
@@ -40,7 +40,8 @@ def parse_formula(raw_formula):
 def get_mw(element):
     return TOKENS[element]
 
-def get_total_mw(formula):
+def get_total_mw(raw_formula):
+    formula = parse_formula(raw_formula)
     total_mw = 0
     for item in formula:
         element, count = item
@@ -50,14 +51,15 @@ def get_total_mw(formula):
 
 def get_mw_by_ds(subst, subst_ratio):
     mw_subst = get_mw(subst)
-    agu_mw = get_total_mw(parse_formula("C6H10O5"))
-    agu_subst_mw = agu_mw + mw_subst - get_total_mw(parse_formula("OH"))
+    agu_mw = get_total_mw("C6H10O5")
+    agu_subst_mw = agu_mw + mw_subst - get_total_mw("OH")
     
     return (1 - subst_ratio) * agu_mw + (subst_ratio * agu_subst_mw)
 
-def get_element_ratios(formula):
+def get_element_ratios(raw_formula):
+    formula = parse_formula(raw_formula)
     element_ratios = list()
-    total_mw = get_total_mw(formula)
+    total_mw = get_total_mw(raw_formula)
     for item in formula:
         element, count = item
         ratio = get_mw(element) * count / total_mw
@@ -72,7 +74,7 @@ def get_ds_c_ratio(subst, c_ratio):
     mw_subst = get_mw(subst)
     return -3 * (54047 * c_ratio - 24022) / ((1000 * mw_subst - 17007) * c_ratio)
 
-def get_ds_x_ratio(mw_subst, x_ratio):
+def get_ds_x_ratio(subst, x_ratio):
     """ Calculates the DS using the ratio of the specified
     substituent according to the elemental analysis."""
     mw_c = get_mw("C")
@@ -81,7 +83,8 @@ def get_ds_x_ratio(mw_subst, x_ratio):
     mw_subst = get_mw(subst)
     return x_ratio * (6 * mw_c + 10 * mw_h + 5 * mw_o) / (mw_subst + x_ratio * (mw_h + mw_o - mw_subst))
 
-def input_elemental_analysis(formula):
+def input_elemental_analysis(raw_formula):
+    formula = parse_formula(raw_formula)
     print("Enter your elemental analysis (in %)")
     print("Leave non determined elements empty!")
     elemental_analysis = list()
@@ -102,12 +105,11 @@ if __name__ == "__main__":
     mw_by_ds = options.mw_by_ds
     x_ds = options.x_ds
     if raw_formula:
-        formula = parse_formula(raw_formula)
-        ratios = get_element_ratios(formula)
+        ratios = get_element_ratios(raw_formula)
         for item in ratios:
             element, ratio = item
             print("{}\t{:.3%}".format(element, ratio))
-        print("Total molweight in g/mol: {0}".format(get_total_mw(formula)))
+        print("Total molweight in g/mol: {0}".format(get_total_mw(raw_formula)))
     if c_ratio and subst:
             print("DS of {}-substituted AGU with C-ratio {}: {:.3}".format(
                 subst, c_ratio, get_ds_c_ratio(subst, float(c_ratio))))
